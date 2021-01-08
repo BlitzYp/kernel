fc8ad8a34f269f23afe84aff918542344abcce3e
codic <dont@email.me>
2021-01-08 09:07:58 -0800

    build.sh: add nasm

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 g++ -c main.cc -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
+nasm -f elf64 boot.s -o boot.o
 ld -T linker.ld -o kernel.bin boot.o kernel.o
 echo "Built, you can now run with QEMU"
